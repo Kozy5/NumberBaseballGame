197a5272290c0bb4efa69f4497c15e9d7a45fd51
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-04-30 12:46:16 +0900

    1차 리팩토링 및 오류해결
    리팩토링
    1. 네이밍 수정
    2. 반복문 별 roof 추가
    3. 초반 인사 문구 function 분리
    
    오류해결
    1. 게임 별 초기화 되어야 할 랜덤 숫자가
    입력 시 마다 초기화 되던 오류 수정
    2. 기록 보기에서 시도 횟수 부분이
    게임 별 다르게 나와야하는데 누적되던 오류 해결

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,69 +4,75 @@ import java.util.*
 import kotlin.concurrent.timer
 
 fun main() {
-
     var countTryGame = 0
-    var countTryNumber = 0
-    var seconds = 0
-    var recordList = mutableListOf<Int>()
-    var recordTimeList = mutableListOf<Int>()
-    var checkEndGame = false
-    while (checkEndGame != true) {
-        seconds = 0
-        countTryNumber = 0
-        println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
-        println("-------------게임 안내---------------")
-        println("투구 요청 안내에 따라 숫자를 입력해주세요 ")
-        println("입력하신 숫자가 존재하면 볼")
-        println("입력하신 숫자의 위치가 정확하면 스트라이크입니다.")
-        println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
-        println("메뉴를 골라주세요")
-        println("1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기")
+    val saveTryNumberList = mutableListOf<Int>()
+    checkEndGame@ while (true) {
+        var countTryNumber = 0
 
-        while (true) {
-            var startInput = readLine()
-            when (startInput) {
+        printGameAndMenuGuide()
+
+        checkStartGame@ while (true) {
+            val getHomeMenuSelectionNumber = readLine()
+            when (getHomeMenuSelectionNumber) {
+
                 "1" -> {
-                    println("게임을 시작합니다")
+                    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
+                    println("투구를 시작해주세요!")
+                    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
                     break
                 }
 
                 "2" -> {
                     if (countTryGame == 0) {
+                        println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
                         println("기록 정보가 없습니다 게임을 시작합니다.")
+                        println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
+                        break@checkStartGame
                     } else {
                         for (i in 0..countTryGame - 1) {
-                            println("${i + 1} 번째 게임 시도 횟수 : ${recordList[i]} 소요시간:${recordTimeList[i]}]등급:챌린저")
+                            println("${i + 1} 번째 게임 시도 횟수 : ${saveTryNumberList[i]}등급:챌린저")
                         }
-                        println("처음 화면으로 돌아가기 1")
-                        val getRecordAndRank = readln()
-                        when (getRecordAndRank) {
-                            "1" -> println("1눌렀을 때")
-                            else -> {
-                                println("-------------------------------------")
-                                println("다시 시작하려면 1번을 입력해주세요")
-                                println("-------------------------------------")
+                        println("1. 게임 시작하기 2. 종료하기")
+                        while (true) {
+                            val getRecordingPageMenuSelectionNumber = readln()
+                            when (getRecordingPageMenuSelectionNumber) {
+                                "1" -> break@checkStartGame
+                                "2" -> break@checkEndGame
+                                else -> {
+                                    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
+                                    println("게임을 시작하려면 1번 종료하시려면 2번을 입력해주세요")
+                                    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
+                                }
                             }
                         }
+
                     }
                 }
 
                 "3" -> {
+                    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
                     println("게임을 종료합니다")
-                    break
+                    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
+                    break@checkEndGame
                 }
 
-                else -> println("올바른 숫자를 입력해주세요!")
+                else -> {
+                    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
+                    println("올바른 숫자를 입력해주세요")
+                    println("1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기")
+                    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
+                }
             }
         }
 
-        var randomNumberString = "0"
-        var checkTest2 = false
-        while (checkTest2 != true) {
-            countTryGame += 1
 
+
+        thisGame@ while (true) {
+            countTryGame += 1
+            var randomNumberString = "0"
             while (randomNumberString.length < 3) {
-                var randomNumber = (Math.random() * 10).toInt().toString()
+                val randomNumber = (Math.random() * 10).toInt().toString()
+
                 if (randomNumberString[0] == '0') {
                     randomNumberString = ""
                     randomNumberString += randomNumber
@@ -75,55 +81,61 @@ fun main() {
                 }
             }
 
-            var checkClearGame = false
-
-            while (checkClearGame != true) {
-                var countStrike = 0
-                var countBall = 0
-                var countWildPitch = 0
-                var checkNumber = false
-
-                while (checkNumber != true) {
-                    timer(period = 1000) {
-                        seconds++
-                        if (seconds % 10 == 0) {
-                            println("${seconds}초 경과")
-                        }
-
-                    }
-
-                    val gameNumber = randomNumberString
-                    println(gameNumber)
-                    val ex = readln()
+            checkInputIsCorrect@ while (true) {
+                val thisGameRandomNumber = randomNumberString
+                while (true) {
+                    println(thisGameRandomNumber)
+                    var countStrike = 0
+                    var countBall = 0
+                    var countWildPitch = 0
                     countTryNumber += 1
-                    for (i in 0..2) {
-                        if (ex[i] == gameNumber[i]) {
-                            countStrike += 1
-                            checkNumber = true
-                        } else if (ex.contains(gameNumber[i])) {
-                            countBall += 1
-                            checkNumber = true
-                        } else if (ex.contains(gameNumber[i]) == false) {
-                            countWildPitch += 1
-                            checkNumber = true
+                    val getInputTryNumber = readln()
+                    if (getInputTryNumber.length == 3) {
+                        for (i in 0..2) {
+                            if (getInputTryNumber[i] == thisGameRandomNumber[i]) {
+                                countStrike += 1
+                            } else if (getInputTryNumber.contains(thisGameRandomNumber[i])) {
+                                countBall += 1
+                            } else if (!getInputTryNumber.contains(thisGameRandomNumber[i])) {
+                                countWildPitch += 1
+                            }
                         }
+                        println("$countStrike 스트라이크 $countBall 볼 $countWildPitch 폭투")
+                    } else {
+                        println()
+                        println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
+
+                        println("투구 방법은 3자리 숫자 입력입니다!")
+                        println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
+                        println()
                     }
-                    println("$countStrike 스트라이크 $countBall 볼 $countWildPitch 폭투")
-
-
                     if (countStrike == 3) {
-                        println("정답입니다!")
-                        checkClearGame = true
-
+                        println()
+                        println("멋진 투구로 삼진 아웃!!!")
+                        println()
+                        break@checkInputIsCorrect
                     }
-                }
-            }
-            checkTest2 = true
-        }
-        recordList += countTryNumber
-        recordTimeList += seconds
 
+                }
+
+
+            }
+            break@thisGame
+        }
+        saveTryNumberList += countTryNumber
     }
+}
+
+fun printGameAndMenuGuide() {
+    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
+    println("-------------게임 안내---------------")
+    println("투구 요청 안내에 따라 숫자를 입력해주세요 ")
+    println("입력하신 숫자가 존재하면 볼")
+    println("입력하신 숫자의 위치가 정확하면 스트라이크입니다.")
+    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
+    println()
+    println("메뉴를 골라주세요")
+    println("1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기")
 }
 
 
