aa94a6fe8625de11ff85dc159b8e23a3798b2960
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-05-01 09:33:18 +0900

    3차 리팩토링
    # 기능분리
    1. 기록 없을 때 기록 요청 시 메세지
    2. 기록,등급(티어)나타내주는 메세지
    3. 그 외 기록 페이지 안내 문구 4개
    4. 게임 종료 메세지
    5. 메인 메뉴 예외 처리 메세지
    6. 랜덤 번호 생성
    7. 판정 별 이미지 생성해주는 반복문
    8. 판정 별 카운트 해주는 반복문
    9. 판정 별 카운트를 받아서 출력해주는 메세지

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,40 +7,22 @@ fun main() {
     var countTryGame = 0
     var grade = ""
     val saveTryNumberList = mutableListOf<Int>()
+
     checkEndGame@ while (true) {
         var countTryNumber = 0
-
         printGameAndMenuGuideMessage()
 
         checkStartGame@ while (true) {
             val getHomeMenuSelectionNumber = readLine()
-
             when (getHomeMenuSelectionNumber) {
                 "1" -> break
                 "2" -> {
                     if (countTryGame == 0) {
-                        println()
-                        println("⚠\uFE0F 기록 정보가 없습니다. 게임을 시작합니다.")
-                        println()
+                        printNothingRecordMessage()
                         break@checkStartGame
                     } else {
-                        for (i in 0..countTryGame - 1) {
-                            when (saveTryNumberList[i]) {
-                                1 -> grade = " \uD83C\uDD91 Challenger"
-                                2 -> grade = " \uD83D\uDEBE World Class"
-                                3 -> grade = " Ⓜ\uFE0F Master"
-                                4 -> grade = """ 💠 Diamond"""
-                                5-> grade = " ❇\uFE0F Emerald"
-                                6 -> grade = " \uD83D\uDFE2 Platinum"
-                                7 -> grade = " \uD83D\uDFE1 Gold"
-                                8 -> grade = " ⬜ Silver"
-                                9,10 -> grade = " \uD83D\uDFEB Bronze"
-                                11,12 -> grade = " ▪\uFE0F Iron"
-                                else -> grade = " ☠\uFE0F 네..?"
-                            }
-                            println("${i + 1} 번째 게임 / 시도 횟수 : ${saveTryNumberList[i]} / 티어:${grade}")
-                        }
-                        println("1.다시 도전하기 2.게임 종료하기\uD83D\uDE4B\u200D♂\uFE0F")
+                        printMessageAboutGradeAndRecord(countTryGame,saveTryNumberList,grade)
+                        printGuideMessageForRetryOrEnd()
                         while (true) {
                             val getRecordingPageMenuSelectionNumber = readln()
                             when (getRecordingPageMenuSelectionNumber) {
@@ -48,22 +30,18 @@ fun main() {
                                     break@checkStartGame
                                 }
                                 "2" -> {
-                                    println("⚠\uFE0F 기록이 초기화됩니다. 정말 종료하시겠습니까?")
-                                    println("1.기록 초기화 및 종료 \uD83E\uDD14 2. 다시 게임 시작하기 \uD83C\uDFC3")
+                                    printCautionMessageAboutInitializeRecord()
                                     val getFinalChoice = readln()
                                     when (getFinalChoice) {
                                         "1" -> break@checkEndGame
                                         "2" -> break@checkStartGame
                                         else -> {
-                                            println("올바른 숫자를 입력해주세요")
-                                            println("정말 종료하시려면 1번 다시 시작하시려면 2번")
+                                            printExceptionHandlingMessageForFinalChoice()
                                         }
                                     }
                                 }
                                 else -> {
-                                    println()
-                                    println("⚾ 게임을 시작하려면 1번 종료하시려면 2번을 입력해주세요")
-                                    println()
+                                    printExceptionHandlingMessageForRecordPageInput()
                                 }
 
                             }
@@ -71,36 +49,20 @@ fun main() {
                     }
                 }
                 "3" -> {
-                    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
-                    println("게임을 종료합니다")
-                    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
+                    printMessageForGameEnd() 
                     break@checkEndGame
                 }
                 else -> {
-                    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
-                    println("올바른 숫자를 입력해주세요")
-                    println("1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기")
-                    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
+                    printExceptionHandlingMessageForHomeMenuInput()
                 }
-
             }
         }
-
         printGameStartMessage()
 
         thisGame@ while (true) {
             countTryGame += 1
-            var randomNumberString = "0"
-            while (randomNumberString.length < 3) {
-                val randomNumber = (Math.random() * 10).toInt().toString()
+            val randomNumberString = randomNumberGenerator()
 
-                if (randomNumberString[0] == '0') {
-                    randomNumberString = ""
-                    randomNumberString += randomNumber
-                } else if (!randomNumberString.contains(randomNumber)) {
-                    randomNumberString += randomNumber
-                }
-            }
             checkInputIsCorrect@ while (true) {
                 val thisGameRandomNumber = randomNumberString
                 while (true) {
@@ -108,40 +70,35 @@ fun main() {
                     var countBall = 0
                     var countWildPitch = 0
                     var imageDisplay = ""
+
                     countTryNumber += 1
                     val getInputTryNumber = readln()
                     if (getInputTryNumber.length == 3) {
-                        for (i in 0..2) {
-                            if (getInputTryNumber[i] == thisGameRandomNumber[i]) {
-                                countStrike += 1
-                            } else if (getInputTryNumber.contains(thisGameRandomNumber[i])) {
-                                countBall += 1
-                            } else if (!getInputTryNumber.contains(thisGameRandomNumber[i])) {
-                                countWildPitch += 1
-                            }
-                        }
-                        while(true){
-                            for(i in 0..countStrike-1){
-                                imageDisplay +="⚾"
-                            }
-                            for(i in 0..countBall-1){
-                                imageDisplay +="\uD83E\uDD4E"
-                            }
-                            for(i in 0..countWildPitch-1){
-                                imageDisplay +="\uD83D\uDCA5"
-                            }
-                            break
-                        }
+                        val countByDecisionType = countByDecisionType(
+                            countStrike,
+                            countBall,
+                            countWildPitch,
+                            getInputTryNumber,
+                            thisGameRandomNumber
+                        )
+                        val makeImageDisplay = makeImageDisplay(
+                            countByDecisionType[0],
+                            countByDecisionType[1],
+                            countByDecisionType[2],
+                            imageDisplay
+                        )
                         println("-----------------------------")
                         println()
-                        println(imageDisplay)
-                        println("$countStrike 스트라이크 / $countBall 볼 / $countWildPitch 폭투")
+                        println(makeImageDisplay)
+                        printCountByDecisionType(countByDecisionType[0],countByDecisionType[1],countByDecisionType[2])
                         println()
                         println("-----------------------------")
 
+                        countStrike = countByDecisionType[0]
                     } else {
                         printHowToPitchGuideMessage()
                     }
+
                     if (countStrike == 3) {
                         printCongratulatoryMessage()
                         break@checkInputIsCorrect
@@ -153,6 +110,44 @@ fun main() {
         saveTryNumberList += countTryNumber
     }
 }
+
+
+
+
+fun printMessageForGameEnd() {
+    println()
+    println("⚾게임을 종료합니다")
+    println()
+}
+
+fun printExceptionHandlingMessageForHomeMenuInput() {
+    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
+    println("올바른 숫자를 입력해주세요")
+    println("1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기")
+    println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
+}
+
+fun printExceptionHandlingMessageForRecordPageInput() {
+    println()
+    println("⚾ 게임을 시작하려면 1번 종료하시려면 2번을 입력해주세요")
+    println()
+}
+
+fun printGuideMessageForRetryOrEnd() {
+    println("1.다시 도전하기 2.게임 종료하기\uD83D\uDE4B\u200D♂\uFE0F")
+}
+
+fun printCautionMessageAboutInitializeRecord() {
+    println("⚠\uFE0F 기록이 초기화됩니다. 정말 종료하시겠습니까?")
+    println("1.기록 초기화 및 종료 \uD83E\uDD14 2. 다시 게임 시작하기 \uD83C\uDFC3")
+}
+
+fun printNothingRecordMessage() {
+    println()
+    println("⚠\uFE0F 기록 정보가 없습니다. 게임을 시작합니다.")
+    println()
+}
+
 fun printGameAndMenuGuideMessage() {
     println("-------------------------------------")
     println("-------------게임 안내---------------")
@@ -168,12 +163,14 @@ fun printGameAndMenuGuideMessage() {
     println("메뉴를 골라주세요")
     println("1. 게임 시작하기 \uD83C\uDFC3 2. 게임 기록 보기 \uD83C\uDFC6 3. 종료하기 \uD83D\uDE4B\u200D♂\uFE0F")
 }
+
 fun printGameStartMessage() {
     println()
     println()
     println("⚾ 투구를 시작해주세요!")
     println("-----------⬇\uFE0F 3자리 숫자 입력 ⬇\uFE0F------------")
 }
+
 fun printCongratulatoryMessage() {
     println()
     println()
@@ -185,10 +182,107 @@ fun printCongratulatoryMessage() {
     println()
     println()
 }
-fun printHowToPitchGuideMessage(){
+
+fun printHowToPitchGuideMessage() {
     println()
     println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
     println("투구 방법은 3자리 숫자 입력입니다!")
     println("⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾⚾")
     println()
+}
+
+fun printExceptionHandlingMessageForFinalChoice() {
+    println("올바른 숫자를 입력해주세요")
+    println("정말 종료하시려면 1번 다시 시작하시려면 2번")
+}
+
+fun giveGradeForTryNumber(num: Int): String {
+    when (num) {
+        1 -> return " \uD83C\uDD91 Challenger"
+        2 -> return " \uD83D\uDEBE World Class"
+        3 -> return " Ⓜ\uFE0F Master"
+        4 -> return """ 💠 Diamond"""
+        5 -> return " ❇\uFE0F Emerald"
+        6 -> return " \uD83D\uDFE2 Platinum"
+        7 -> return " \uD83D\uDFE1 Gold"
+        8 -> return " ⬜ Silver"
+        9, 10 -> return " \uD83D\uDFEB Bronze"
+        11, 12 -> return " ▪\uFE0F Iron"
+        else -> return " ☠\uFE0F 네..?"
+    }
+}
+
+fun randomNumberGenerator(): String {
+    var randomNumberString = "0"
+    while (randomNumberString.length < 3) {
+        val randomNumber = (Math.random() * 10).toInt().toString()
+        if (randomNumberString[0] == '0') {
+            randomNumberString = ""
+            randomNumberString += randomNumber
+        } else if (!randomNumberString.contains(randomNumber)) {
+            randomNumberString += randomNumber
+        }
+    }
+    return randomNumberString
+}
+
+fun countByDecisionType(
+    strike: Int,
+    ball: Int,
+    wildPitch: Int,
+    tryNumber: String,
+    randomNumber: String
+): MutableList<Int> {
+    var countByDecisionTypeList = mutableListOf<Int>()
+    var countStrike = strike
+    var countBall = ball
+    var countWildPitch = wildPitch
+    if (tryNumber.length == 3) {
+        for (i in 0..2) {
+            if (tryNumber[i] == randomNumber[i]) {
+                countStrike += 1
+            } else if (tryNumber.contains(randomNumber[i])) {
+                countBall += 1
+            } else if (!tryNumber.contains(randomNumber[i])) {
+                countWildPitch += 1
+            }
+        }
+    } else {
+        printHowToPitchGuideMessage()
+    }
+    countByDecisionTypeList += countStrike
+    countByDecisionTypeList += countBall
+    countByDecisionTypeList += countWildPitch
+    return countByDecisionTypeList
+}
+
+fun makeImageDisplay(strike: Int, ball: Int, wildPitch: Int, imageDisplay: String): String {
+    var imgdp = imageDisplay
+    val countStrike = strike
+    val countBall = ball
+    val countWildPitch = wildPitch
+    while (true) {
+        for (i in 0..countStrike - 1) {
+            imgdp += "⚾"
+        }
+        for (i in 0..countBall - 1) {
+            imgdp += "\uD83E\uDD4E"
+        }
+        for (i in 0..countWildPitch - 1) {
+            imgdp += "\uD83D\uDCA5"
+        }
+        break
+    }
+    return imgdp
+}
+
+fun printCountByDecisionType(strike: Int, ball: Int, wildPitch: Int) {
+    println("$strike 스트라이크 / $ball 볼 / $wildPitch 폭투 ")
+}
+fun printMessageAboutGradeAndRecord(tryGame:Int,saveTryNumberList: MutableList<Int>,grade:String){
+    var grade = grade
+    for (i in 0..tryGame - 1) {
+        grade = giveGradeForTryNumber(saveTryNumberList[i])
+        println("${i + 1} 번째 게임 / 시도 횟수 : ${saveTryNumberList[i]} / 티어:${grade}")
+    }
 }
